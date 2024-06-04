49b3c878699197023322382b2787b7f70818f9cf
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-06-04 09:06:45 -0500

    Mi primer comit

---
+++ Config/Config.php
@@ -0,0 +1,15 @@
+<?php 
+	const BASE_URL = "http://localhost/basesis";
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+	//Simbolo de moneda
+	const SMONEY = "S/.";
---
+++ Views/APP/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Mi framework</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
