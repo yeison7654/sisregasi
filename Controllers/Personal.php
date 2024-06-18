c88efb2a7a4f7f3e845e991664a8a1f8d4dbaeee
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-06-18 09:23:35 -0500

    creacion formulario registro personal

---
+++ Controllers/Personal.php
@@ -0,0 +1,18 @@
+<?php
+class Personal extends Controllers
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function newpersonal()
+    {
+        $data['page_id'] = 1;
+        $data['page_tag'] = "Nuevo Personal";
+        $data['page_title'] = "Registro de nuevo personal";
+        $data['page_name'] = "newpersonal";
+        $data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
+        $this->views->getView($this, "newpersonal", $data);
+    }
+}
--- Views/Template/cpanel/menu-vertical.php
+++ Views/Template/cpanel/menu-vertical.php
@@ -1,5 +1,4 @@
   <!-- Menu -->
-
   <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
       <div class="app-brand demo">
           <a href="index.html" class="app-brand-link">
