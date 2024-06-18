7dfc02d60a8636d2eaad578a34a43d0fe3cb9e37
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-06-18 09:44:12 -0500

    formulario registro personal

--- Views/APP/Personal/newpersonal.php
+++ Views/APP/Personal/newpersonal.php
@@ -13,30 +13,53 @@ headerAdmin($data);
             <div class="card-body">
                 <form>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-fullname">Full Name</label>
+                        <label class="form-label" for="basic-default-fullname">Nombres</label>
                         <input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-company">Company</label>
+                        <label class="form-label" for="basic-default-fullname">Apellidos</label>
+                        <input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe">
+                    </div>
+                    <div class="mb-3">
+                        <label for="defaultSelect" class="form-label">Estado Civil</label>
+                        <select id="defaultSelect" class="form-select">
+                            <option>Default select</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-company">Fecha Nacimiento</label>
+                        <input type="date" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                    </div>
+                    <div class="mb-3">
+                        <label for="defaultSelect" class="form-label">Tipo Personal</label>
+                        <select id="defaultSelect" class="form-select">
+                            <option>Default select</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-company">Celular</label>
+                        <input type="number" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-company">Codigo Personal</label>
                         <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-email">Email</label>
-                        <div class="input-group input-group-merge">
-                            <input type="text" id="basic-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-default-email2">
-                            <span class="input-group-text" id="basic-default-email2">@example.com</span>
-                        </div>
-                        <div class="form-text">You can use letters, numbers &amp; periods</div>
+                        <label for="defaultSelect" class="form-label">Ocupacion</label>
+                        <select id="defaultSelect" class="form-select">
+                            <option>Default select</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-default-phone">Phone No</label>
-                        <input type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="658 799 8941">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-default-message">Message</label>
-                        <textarea id="basic-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Send</button>
+                    <button type="submit" class="btn btn-primary">Registrar</button>
                 </form>
             </div>
         </div>
